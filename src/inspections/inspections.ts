656745765d6184dc21b90a36f286aa9565beb9c1
mazatf2 <2814748+mazatf2@users.noreply.github.com>
2020-07-23 12:02:03 +0300

    feat: implement core inspections

--- src/inspections/inspections.ts
+++ src/inspections/inspections.ts
@@ -1,24 +1,59 @@
-import {valid, compare} from 'semver'
-import {customFolderResource} from '../../tools/generate_config'
+import {compare, valid} from 'semver'
+import {assetType, customFolderResource} from '../../tools/generate_config'
 
 export type inspectionObj = {
 	inspection: string
 	path: string
+	assetType: assetType
 }
 
-export const Inspections = (customFolderResources: customFolderResource[] | null, ghResources:  customFolderResource[] | null) => {
+export const Inspections = (customFolderResources: customFolderResource[] | null, ghResources: customFolderResource[] | null): inspectionObj[] => {
 	const inspections: inspectionObj[] = []
 
-	if(!customFolderResources || !ghResources) {
+	if (!customFolderResources || !ghResources) {
 		console.error('!filesystemAssets')
 		return inspections
 	}
 
 	for (const i of customFolderResources) {
-		const result = no_old_versions(i, ghResources)
+		let results = [
+			no_old_versions(i, ghResources),
+			no_multiple_same_resources(i, customFolderResources),
+		]
 
-		if (result?.inspection) inspections.push(result)
+		results = results.filter(r => r && r?.inspection)
+
+		if (results)
+			inspections.push(...results)
 	}
+
+	const presets = customFolderResources
+		.filter(i => i.assetType === 'preset')
+
+	if (inspections.some(i => i.inspection === 'no_multiple_same_resources')) {
+		let results = presets
+			.map(i => no_multiple_different_quality_presets(i))
+
+		inspections.push(...results)
+	}
+
+	let activePreset
+	// for marking preset as installed
+	if (presets.length === 1 && can_mark_as_active_preset(presets[0], inspections)) {
+		activePreset = mark_as_active_preset(presets[0])
+
+		inspections.push(activePreset)
+	}
+
+	if (activePreset) {
+		let results = []
+		//no_presets_without_recommended_addons()
+
+		///if (result) inspections.push(...result)
+
+	}
+
+	console.log(inspections)
 
 	return inspections
 }
@@ -26,26 +61,84 @@ export const Inspections = (customFolderResources: customFolderResource[] | null
 const no_old_versions = (i: customFolderResource, ghResources: customFolderResource[]) => {
 	const latest = ghResources[0]
 	const latestVersion = valid(latest.version)
-	if (!latestVersion) return 'version check err'
+	if (!latestVersion) throw 'version check err'
 
 	if (i.assetType === 'preset') {
 		const ourVersion = valid(i.version)
-		if (!ourVersion) return 'version err'
+		if (!ourVersion) throw 'version err'
 
 		const isOursOld = compare(latestVersion, ourVersion)
 		if (isOursOld === 1) {
 			console.log('old')
+
 			return {
 				inspection: 'no_old_versions',
 				path: i.path,
+				assetType: i.assetType,
 			} as inspectionObj
 		}
 	}
+
+	return null
 }
 
-// etc: no more than 1 preset installed. low, high
-const no_multiple_same_resources = (i: customFolderResource, ghResources: customFolderResource[]) => {
+// case: multiple different quality presets installed. e.g low, low, high
+const no_multiple_different_quality_presets = (i: customFolderResource) => {
+	if (i.assetType !== 'preset') throw 'wrong resource type'
 
+	return {
+		inspection: 'no_multiple_different_quality_presets',
+		path: i.path,
+		assetType: i.assetType,
+	} as inspectionObj
+}
+
+// e.g: no more than 1 preset installed. low, high
+const no_multiple_same_resources = (i: customFolderResource, all: customFolderResource[]) => {
+	if (i.assetType !== 'preset') return null
+
+	// name = high, autoexec
+	const duplicates = all
+		.filter(r => r.name === i.name)
+		.length
+
+	if (duplicates > 1) {
+		return {
+			inspection: 'no_multiple_same_resources',
+			path: i.path,
+			assetType: i.assetType,
+		} as inspectionObj
+	}
+
+	return null
+}
+
+const no_presets_without_recommended_addons = (i: customFolderResource) => {
+
+}
+
+// block marking as active preset
+const blockingArr = ['no_multiple_same_resources']
+
+const can_mark_as_active_preset = (i: customFolderResource, inspections: inspectionObj[]) => {
+	if (i.assetType !== 'preset') throw 'wrong resource type'
+
+	const hasBlocking = inspections.some(i => blockingArr.includes(i.inspection))
+
+	if (hasBlocking) return false
+
+	return true
+}
+
+// installed & active preset
+const mark_as_active_preset = (i: customFolderResource) => {
+	if (i.assetType !== 'preset') throw 'wrong resource type'
+
+	return {
+		inspection: 'active_preset',
+		path: i.path,
+		assetType: i.assetType,
+	} as inspectionObj
 }
 
 const no_autoexec_loops = (i: any) => {
